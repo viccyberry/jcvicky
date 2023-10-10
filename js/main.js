498f39fd7d208114002172a1f82355ffc0f56a45
viccyberry <143361577+viccyberry@users.noreply.github.com>
2023-10-10 21:18:32 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,68 @@
+const year = document.getElementById("year")
+const thisYear = new Date().getFullYear()
+year.setAttribute("datatime", thisYear)
+year.textContent = thisYear
+
+
+
+var menubtn = document.getElementById ("menubtn")
+var navbarlink = document.getElementById ("navbarlink")
+var menu = document.getElementById ("menu")
+
+menubtn.onclick = function (){
+    if (navbarlink.style.right == "-250px"){
+        navbarlink.style.right = "0";
+    }
+    else {
+        navbarlink.style.right = "-250px";  
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function sendMail() {
+//     var params = {
+//         name: document.getElementById("name").value,
+//         name: document.getElementById("email").value,
+//         name: document.getElementById("message").value,
+//     };
+
+//     const serviceID = "service_cmjmsoa";
+//     const templateID = "template_c2wqtqc";
+
+//     emailjs
+//     .send(serviceID, templateID, params)
+//     .then((res) => {
+       
+//             document.getElementById("name").value = "";
+//             document.getElementById("email").value = "";
+//             document.getElementById("message").value = "";
+//             console.log(res);
+//             alert("your message sent successfully");
+//         }
+//     )
+//     .catch((err) => console.log(err));
+// }
